2eac937fcdf956cae5ed9e9cf7a618bccc445967
cdmccune <curtmccune@gmail.com>
2022-06-03 08:53:43 -0700

    Initial Commit

---
+++ EventManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventManager
+//
+//  Created by Curt McCune on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
